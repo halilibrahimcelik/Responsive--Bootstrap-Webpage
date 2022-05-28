6c27c50fd4c4c6ccdf417254299842eefcd76911
halilibrahimcelik <you@example.com>
2022-05-29 01:53:33 +0300

    Adding  Courses section.

---
+++ app.js
@@ -0,0 +1,7 @@
+// import "/lib/fslightbox-basic-3.3.1/fslightbox";
+
+// const fullStackBtn = document.getElementById("fullStackBtn");
+
+// fullStackBtn.addEventListener("click", () => {
+//   fsLightboxInstances["gallery"].open(0);
+// });

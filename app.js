1c10385ce076447c87c0e9602203615b5aef7d28
halilibrahimcelik <you@example.com>
2022-05-29 09:30:22 +0300

    Adding footer and map logic.

--- app.js
+++ app.js
@@ -5,3 +5,33 @@
 // fullStackBtn.addEventListener("click", () => {
 //   fsLightboxInstances["gallery"].open(0);
 // });
+
+class Map {
+  constructor() {
+    this.coordinates = {
+      lat: 39.897174,
+      lng: 32.771146,
+    };
+
+    this.render();
+  }
+
+  render() {
+    if (!google) {
+      alert("Could not load maps library, please try again later");
+      return;
+    }
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: this.coordinates,
+      zoom: 16,
+    });
+
+    new google.maps.Marker({
+      position: this.coordinates,
+      map: map,
+    });
+  }
+}
+
+new Map();

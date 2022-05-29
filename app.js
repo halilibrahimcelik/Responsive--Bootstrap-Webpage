b9e77e0172fb908e5d38b44870619301c64138c6
halilibrahimcelik <you@example.com>
2022-05-29 09:40:27 +0300

    Regulating JS code.

--- app.js
+++ app.js
@@ -1,10 +1,65 @@
-// import "/lib/fslightbox-basic-3.3.1/fslightbox";
+const fullStackBtn = document.querySelector(".fullStackBtn");
+const dataScience = document.querySelector(".dataScience");
+const devOps = document.querySelector(".devOps");
 
-// const fullStackBtn = document.getElementById("fullStackBtn");
+const dsImagesRow = document.querySelector(".dsImages");
+const fsImagesRow = document.querySelector(".fsImages");
+const opsImageRow = document.querySelector(".opsImages");
+let isVisible = false;
 
-// fullStackBtn.addEventListener("click", () => {
-//   fsLightboxInstances["gallery"].open(0);
-// });
+fullStackBtn.addEventListener("click", () => {
+  fsLightboxInstances["gallery"].open(1);
+  if (!isVisible) {
+    isVisible = true;
+    dsImagesRow.classList.add("active-visible");
+    opsImageRow.classList.add("active-visible");
+    fsImagesRow.classList.remove("active-visible");
+  } else {
+    isVisible = false;
+    opsImageRow.classList.add("active-visible");
+    dsImagesRow.classList.add("active-visible");
+    fsImagesRow.classList.remove("active-visible");
+  }
+  console.log(isVisible);
+});
+
+dataScience.addEventListener("click", () => {
+  let visible = false;
+  fsLightboxInstances["gallery"].open(0);
+  if (!visible) {
+    visible = true;
+    dsImagesRow.classList.remove("active-visible");
+    opsImageRow.classList.add("active-visible");
+    fsImagesRow.classList.add("active-visible");
+  } else {
+    visible = false;
+    fsImagesRow.classList.add("active-visible");
+    opsImageRow.classList.add("active-visible");
+    dsImagesRow.classList.remove("active-visible");
+  }
+
+  console.log(visible);
+});
+devOps.addEventListener("click", () => {
+  fsLightboxInstances["gallery"].open(0);
+
+  let visibility = false;
+
+  if (!visibility) {
+    visibility = true;
+    opsImageRow.classList.remove("active-visible");
+    fsImagesRow.classList.add("active-visible");
+    dsImagesRow.classList.add("active-visible");
+  } else {
+    visibility = false;
+    opsImageRow.classList.remove("active-visible");
+    fsImagesRow.classList.add("active-visible");
+    dsImagesRow.classList.add("active-visible");
+  }
+  console.log(visibility);
+});
+
+console.log(isVisible);
 
 class Map {
   constructor() {
